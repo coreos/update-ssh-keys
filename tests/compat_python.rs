d1d9751f931bff0ad41dc65f6fc00d9132624fe2
Luca Bruno <lucab@debian.org>
2017-10-20 11:46:44 +0000

    tests: wire old python testsuite into cargo tests
    This adds some gluing logic in order to let `cargo test` also run the
    existing python testsuite.

---
+++ tests/compat_python.rs
@@ -0,0 +1,20 @@
+use std::env;
+use std::process::Command;
+
+// This runs the old python integration test-suite to ensure
+// retro-compatibility.
+#[test]
+fn test_compat_python_suite() {
+    let pytests = env::current_dir().unwrap()
+        .join("tests")
+        .join("test_update_ssh_keys.py");
+    let result = Command::new(pytests).output().unwrap();
+    if !result.status.success() {
+        panic!(format!(
+            "\nstdout: {}\nstderr: {}",
+            String::from_utf8_lossy(&result.stdout),
+            String::from_utf8_lossy(&result.stderr)
+        ));
+    };
+    assert!(result.status.success());
+}
